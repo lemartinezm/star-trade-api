25f3b49041c867bc0661713edcd5b6ab1487b95f
Luis Martínez <lemartinezmendoza@gmail.com>
2023-12-05 12:41:47 -0500

    chore: update expiration time for jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,6 +13,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '2h' },
       }),
       inject: [ConfigService],
     }),
