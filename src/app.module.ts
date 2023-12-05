65aa54dbcd71eedab12b9070cbd51ca30ad972b5
Luis Martínez <lemartinezmendoza@gmail.com>
2023-12-05 08:04:44 -0500

    chore: update mysql port

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { AppService } from './app.service';
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.DB_HOST,
-      port: 3306,
+      port: parseInt(process.env.PORT) || 3306,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
