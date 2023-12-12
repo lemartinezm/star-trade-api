2da24287b58266bb568f83891ef810a3124777eb
Luis Martínez <lemartinezmendoza@gmail.com>
2023-12-12 11:20:55 -0500

    fix: add cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // TODO: add url to cors for prod
   await app.listen(3000);
 }
 bootstrap();
